f053002c69629068e5635c76699e4c9b5ee6dc12
Ben <bneaton121@gmail.com>
2021-03-19 11:52:51 +0000

    new file:   DSP/.gitignore 	new file:   DSP/.idea/.gitignore 	new file:   DSP/.idea/compiler.xml 	new file:   DSP/.idea/gradle.xml 	new file:   DSP/.idea/jarRepositories.xml 	new file:   DSP/.idea/misc.xml 	new file:   DSP/app/.gitignore 	new file:   DSP/app/build.gradle 	new file:   DSP/app/proguard-rules.pro 	new file:   DSP/app/src/androidTest/java/com/example/dsp/ExampleInstrumentedTest.java 	new file:   DSP/app/src/main/AndroidManifest.xml 	new file:   DSP/app/src/main/java/com/example/dsp/Alerts.java 	new file:   DSP/app/src/main/java/com/example/dsp/Edit.java 	new file:   DSP/app/src/main/java/com/example/dsp/Library.java 	new file:   DSP/app/src/main/java/com/example/dsp/MainMenu.java 	new file:   DSP/app/src/main/java/com/example/dsp/data/LoginDataSource.java 	new file:   DSP/app/src/main/java/com/example/dsp/data/LoginRepository.java 	new file:   DSP/app/src/main/java/com/example/dsp/data/Result.java 	new file:   DSP/app/src/main/java/com/example/dsp/data/model/LoggedInUser.java 	new file:   DSP/app/src/main/java/com/example/dsp/liveVideoFeed.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoggedInUserView.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoginActivity.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoginFormState.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoginResult.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoginViewModel.java 	new file:   DSP/app/src/main/java/com/example/dsp/ui/login/LoginViewModelFactory.java 	new file:   DSP/app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   DSP/app/src/main/res/drawable/ic_launcher_background.xml 	new file:   DSP/app/src/main/res/layout/.directory 	new file:   DSP/app/src/main/res/layout/activity_alerts.xml 	new file:   DSP/app/src/main/res/layout/activity_edit.xml 	new file:   DSP/app/src/main/res/layout/activity_library.xml 	new file:   DSP/app/src/main/res/layout/activity_live_video_feed.xml 	new file:   DSP/app/src/main/res/layout/activity_login.xml 	new file:   DSP/app/src/main/res/layout/activity_main_menu.xml 	new file:   DSP/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   DSP/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   DSP/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   DSP/app/src/main/res/mipmap-hdpi/ic_launcher_round.png 	new file:   DSP/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   DSP/app/src/main/res/mipmap-mdpi/ic_launcher_round.png 	new file:   DSP/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   DSP/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png 	new file:   DSP/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   DSP/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png 	new file:   DSP/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   DSP/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png 	new file:   DSP/app/src/main/res/values-night/themes.xml 	new file:   DSP/app/src/main/res/values/attrs.xml 	new file:   DSP/app/src/main/res/values/colors.xml 	new file:   DSP/app/src/main/res/values/dimens.xml 	new file:   DSP/app/src/main/res/values/strings.xml 	new file:   DSP/app/src/main/res/values/styles.xml 	new file:   DSP/app/src/main/res/values/themes.xml 	new file:   DSP/app/src/test/java/com/example/dsp/ExampleUnitTest.java 	new file:   DSP/build.gradle 	new file:   DSP/gradle.properties 	new file:   DSP/gradle/wrapper/gradle-wrapper.jar 	new file:   DSP/gradle/wrapper/gradle-wrapper.properties 	new file:   DSP/gradlew 	new file:   DSP/gradlew.bat 	new file:   DSP/settings.gradle

---
+++ DSP/app/src/main/java/com/example/dsp/MainMenu.java
@@ -0,0 +1,53 @@
+package com.example.dsp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.dsp.R;
+import com.example.dsp.ui.login.LoginActivity;
+
+
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        //import buttons
+        final Button liveVideoFeedButton = findViewById(R.id.liveVideoFeed);
+        final Button libraryButton = findViewById(R.id.videoLibrary);
+        final Button alertsButton = findViewById(R.id.viewAlerts);
+        final Button editButton = findViewById(R.id.editAlertPeriod);
+
+        //Code for directing a user to diff page depending on what is selected
+        liveVideoFeedButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainMenu.this, liveVideoFeed.class);
+            startActivity(intent);
+        });
+        libraryButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainMenu.this, Library.class);
+            startActivity(intent);
+        });
+        alertsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainMenu.this, Alerts.class);
+            startActivity(intent);
+        });
+        editButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainMenu.this, Edit.class);
+            startActivity(intent);
+        });
+
+
+
+
+
+    }
+
+}
