fc82444a7c8c2db356efd31a70ccd23430619fd6
Ben Eaton <bneaton121@gmail.com>
2021-03-25 14:20:52 +0000

    Update GPIO-sensors.py
    Added automatic snapshot taking

--- GPIO-sensors.py
+++ GPIO-sensors.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(13, GPIO.IN)         #Read output from magnetic door switch
-
+snapshot = 'DATE=$(date +"%Y-%m-%d_%H:%M") && fswebcam -r 1280x720 /var/www/media/photos/$DATE.jpg' #Take snapshot 
 while True:
     #Set variables
     PIR=GPIO.input(11)
@@ -13,14 +14,18 @@ while True:
     time.sleep(0.25)
     if PIR==1: #if PIR sensor is high write to log file
         t = ("PERSON DETECTED AT ",time.asctime())
-        log = open("log.txt", "a")
+        #Append warning message to log file
+        log = open("html/log.txt", "a")
         log.writelines(t)
         log.write('\n')
         log.close()
+        os.system(snapshot)
+
     if MAG==1: #if MAG switch is high write to log file
         t = ("MAG OPEN AT ",time.asctime())
-        log = open("log.txt", "a")
+        #Append warning message to log file
+        log = open("html/log.txt", "a")
         log.writelines(t)
         log.write('\n')
         log.close()
-
+        os.system(snapshot)
