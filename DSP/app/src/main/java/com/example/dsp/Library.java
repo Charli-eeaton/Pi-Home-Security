dab2b20a3226f91f6b5a8c7b8af06c41bd8b2949
Ben Eaton <bneaton121@gmail.com>
2021-03-31 14:38:11 +0100

    Update Library.java

--- DSP/app/src/main/java/com/example/dsp/Library.java
+++ DSP/app/src/main/java/com/example/dsp/Library.java
@@ -3,6 +3,7 @@ package com.example.dsp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Library extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Library extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        //Set webview to media folder
+        WebView activity_library = (WebView) findViewById(R.id.media);
+        activity_library.loadUrl("http://192.168.0.29/library");
     }
 }
